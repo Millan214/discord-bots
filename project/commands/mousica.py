ba2d26c4c7060ded21422d4da51a9429425f1137
Millan Martinez <millan.martinez.arconada.2001@gmail.com>
2024-10-30 20:18:18 +0100

    AUDIO: Im under the water

--- project/commands/mousica.py
+++ project/commands/mousica.py
@@ -15,7 +15,8 @@ sounds = {
     "exploto": "exploto.mp3",
     "pal lobby": "pal-lobby.mp3",
     "pal lobby yo tambien": "pal-lobby-yo-tambien.mp3",
-    "re manco el maxi": "re-manco-el-maxi.mp3"
+    "re manco el maxi": "re-manco-el-maxi.mp3",
+    "im under the water": "im-under-the-water.mp3"
 }
 
 # Create a Button interaction for each sound
